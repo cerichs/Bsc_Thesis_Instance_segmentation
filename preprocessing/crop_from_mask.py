c28b848dd55cecb8a3898e40662dcf2e7501254e
Cornelius E <s204085@student.dtu.dk>
2023-02-14 16:57:14 +0100

    Added documentation

--- preprocessing/crop_from_mask.py
+++ preprocessing/crop_from_mask.py
@@ -10,7 +10,24 @@ import matplotlib.pyplot as plt
 import cv2 as cv
 from skimage.draw import polygon
 
-def crop_from_mask(bbox,cropped_im,dataset):
+def crop_from_mask(bbox,cropped_im):
+    """Crops the region of interest in the cropped_im image
+    
+    Parameters
+    ----------
+    bbox : list, [x,y,width,height]
+        The bounding box from the COCO dataset.
+    cropped_im : Array-like image
+        The image from fill_mask, where the background is removed and only
+        region of intereset is shown, rest is black.
+
+    Returns
+    -------
+    cropped : Array-like image
+        Returns the cropped image with only the region of interest of dimension
+        x*y
+
+    """
     bbox=np.int16(bbox)
 
     start_x = bbox[1]
@@ -23,6 +40,24 @@ def crop_from_mask(bbox,cropped_im,dataset):
     return cropped
 
 def fill_mask(image_id,annotation,image_name):
+    """ Takes the segmentations from COCO dataset and discards the background 
+    (ie. the region that is not withing interest)
+
+    Parameters
+    ----------
+    image_id : int
+        The image_id from the COCO dataset
+    annotation : list, [x,y,x,y...x,y]
+        The segmentation mask obtained from the COCO dataset
+    image_name : str
+        The name of the image where the mask orignates from
+
+    Returns
+    -------
+    cropped_im : Returns the cropped image, where the area of interest has
+    been cropped out and put on a black background
+
+    """
     height = dataset['images']['id'==image_id]['height']
     width = dataset['images']['id'==image_id]['width']
     mini_img = np.zeros((height,width),dtype=bool)
@@ -41,13 +76,32 @@ def fill_mask(image_id,annotation,image_name):
     return cropped_im
 
 def overlay_on_larger_image(larger_image,smaller_image,x_offset=None,y_offset=None):
+    """ Place an object from a mask upon an image that is larger or same size as the object image
+    Parameters
+    ----------
+    larger_image : array-like
+        The background image to be overlayed upon
+    smaller_image : array-like
+        The object to be placed on the larger_image
+    x_offset : int. Optional
+        Where to place the object in respect to origo on the x-axis
+        Optional. The default is None.
+    y_offset : int. Optional
+        Where to place the object in respect to origo on the y-axis
+        The default is None.
+
+    Returns
+    -------
+    Displays the resulting image.
+
+    """
     if x_offset == None:
         x = np.random.randint(0,larger_image.shape[1])
     if y_offset == None:
         y = np.random.randint(0,larger_image.shape[0])
-    temp = larger_image[y:y+smaller_image.shape[0], x:x+smaller_image.shape[1]]
-    temp[cropped>0] = 0
-    temp += cropped*(cropped > 0)
+    temp = larger_image[y:y+smaller_image.shape[0], x:x+smaller_image.shape[1]] # Selecting a window of the image to edit
+    temp[cropped>0] = 0 # All the places where the object is, is set to 0 (the background around mask is unchanged)
+    temp += cropped * (cropped > 0) 
     larger_image[y:y+smaller_image.shape[0], x:x+smaller_image.shape[1]] = temp
     plt.imshow(larger_image)
     plt.show()
